a7b94d8b14bd1ab79cefd85d5c253db3a4ddbffb
kaaang <kaaang@kakao.com>
2022-08-19 01:17:40 +0900

    first

--- src/test/java/study/querydsl/entity/HelloTest.java
+++ src/test/java/study/querydsl/entity/HelloTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,7 +17,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @Transactional
 class HelloTest {
 
-    @Autowired
+    @PersistenceContext
     EntityManager em;
 
     @Test
